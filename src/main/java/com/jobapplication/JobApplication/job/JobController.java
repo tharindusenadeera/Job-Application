b8477223f7adfdc135e4f440b3b5d3175379af32
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-07-12 23:18:57 +0100

    Job create request initiated

--- src/main/java/com/jobapplication/JobApplication/job/JobController.java
+++ src/main/java/com/jobapplication/JobApplication/job/JobController.java
@@ -1,6 +1,8 @@
 package com.jobapplication.JobApplication.job;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,5 +16,11 @@ public class JobController {
     @GetMapping("/jobs")
     public List<Job> findAll() {
         return jobs;
+    }
+
+    @PostMapping("/jobs")
+    public String createJobs(@RequestBody Job job){
+        jobs.add(job);
+        return "Job Added Successfully!";
     }
 }
