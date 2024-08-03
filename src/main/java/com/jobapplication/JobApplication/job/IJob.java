8087a71a47d60fea907ce107a77e28b6e1f036ff
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-08-03 23:57:42 +0100

    Getting a single job API done

--- src/main/java/com/jobapplication/JobApplication/job/IJob.java
+++ src/main/java/com/jobapplication/JobApplication/job/IJob.java
@@ -1,8 +1,11 @@
 package com.jobapplication.JobApplication.job;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IJob {
     List<Job> findAll();
     void createJobs(Job job);
+
+    Job getJobById(Long id);
 }
